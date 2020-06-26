d572cc3fd5bc242ba9e549535ef08b6070ce0b37
Stephanie Castillo <stephaanie.castillo@gmail.com>
2020-06-26 11:15:54 -0300

    creacion de la carpeta models, security, y un nuevo service y las clases ademas de la aplication.properties

---
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/models/LoginRequest.java
@@ -0,0 +1,8 @@
+package ar.com.ada.api.billeteravirtual.models;
+
+public class LoginRequest {
+    
+    public String username;
+
+    public String password;
+}
---
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/models/RegistrationRequest.java
@@ -0,0 +1,19 @@
+package ar.com.ada.api.billeteravirtual.models;
+
+import java.util.Date;
+
+public class RegistrationRequest {
+    
+    public String fullName;
+    
+    public int country;
+
+    public int identificationType;
+
+    public Date birthDate;
+
+    public String email;
+
+    public String password;
+    
+}
