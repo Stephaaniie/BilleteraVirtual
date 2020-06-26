ccd6151868588c2be2bd4c901520b63315f81ce9
Stephanie Castillo <stephaanie.castillo@gmail.com>
2020-06-26 12:34:19 -0300

    modificacion en la clse JWTRequesrFilter  se le borro los comentarios

--- billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/security/jwt/JWTRequestFilter.java
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/security/jwt/JWTRequestFilter.java
@@ -18,9 +18,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import ar.com.ada.api.billeteravirtual.services.JWTUserDetailsService;
 import io.jsonwebtoken.ExpiredJwtException;
 
-/**
- * Este es un filtro que se ejectue en cada request UNA sola ves(OncePerRequest)
- */
 @Component
 public class JWTRequestFilter extends OncePerRequestFilter {
 
@@ -31,9 +28,7 @@ public class JWTRequestFilter extends OncePerRequestFilter {
     private JWTTokenUtil jwtTokenUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
 
         final String requestTokenHeader = request.getHeader("Authorization");
 
@@ -41,8 +36,6 @@ public class JWTRequestFilter extends OncePerRequestFilter {
 
         String jwtToken = null;
 
-        // JWT Token viene de la forma "Bearer token". Remover el texto Bearer y obtener
-        // solo el Token
         if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
 
             jwtToken = requestTokenHeader.substring(7);
@@ -50,51 +43,22 @@ public class JWTRequestFilter extends OncePerRequestFilter {
             try {
                 username = jwtTokenUtil.getUsernameFromToken(jwtToken);
             } catch (IllegalArgumentException e) {
-
                 System.out.println("Unable to get JWT Token");
-
             } catch (ExpiredJwtException e) {
-
                 System.out.println("JWT Token has expired");
-
             }
-
         } else {
-
             logger.warn("JWT Token does not begin with Bearer String");
-
         }
 
-        // una vez obtenido, validarlo.
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
             UserDetails userDetails = this.jwtUserDetailsService.loadUserByUsername(username);
-
-            // si el token es valido, configurar Spring Security para poner la autenticacion
-            // manualmente
-
             if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
-
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-
-                        userDetails, null, userDetails.getAuthorities());
-
-                usernamePasswordAuthenticationToken
-
-                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                // Despues de setear el Authentication en el contexto, especificaremos
-                // que el usuario esta autenticado. y se pasa a
-                // Spring Security Configurations en forma exitosa.
-
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-
             }
-
         }
-
         chain.doFilter(request, response);
-
     }
-
 }
