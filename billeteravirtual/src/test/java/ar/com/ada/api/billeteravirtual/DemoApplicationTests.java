6b159b1b115e648b67289e6d455851e6bf5135ef
Stephanie Castillo <stephaanie.castillo@gmail.com>
2020-07-16 12:07:17 -0300

    Se modificaron los test

--- billeteravirtual/src/test/java/ar/com/ada/api/billeteravirtual/DemoApplicationTests.java
+++ billeteravirtual/src/test/java/ar/com/ada/api/billeteravirtual/DemoApplicationTests.java
@@ -65,46 +65,4 @@ class DemoApplicationTests {
 
 		assertTrue(textoHasheado.equals(hashEsperado));
 	}
-	@Test
-	void CrearUsuarioTest() {
-
-
-		Usuario usuario = usuarioService.crearUsuario("Karen", ARGENTINA, DNI , "21231123", new Date(), "karen@gmail.com", "a12345");
-		
-		//System.out.println("SALDO de usuario: " + usuario.getPersona().getBilletera().getCuenta("ARS").getSaldo());
-
-		//Usuario usuarioVerificado = usuarioService.buscarPorUsername(usuario.getUsername());
-
-		//assertTrue(usuario.getUsuarioId() == usuarioVerificado.getUsuarioId());
-		assertTrue(usuario.getUsuarioId()==1);
-		assertTrue(usuario.getPersona().getBilletera().getCuentaPorMoneda("ARS").getSaldo().equals(new BigDecimal(500)));
-	
-	}
-
-	@Test
-	void EnviarSaldoTest() {
-		
-		Usuario usuarioEmisor = usuarioService.crearUsuario("Karen Envia", 32, 5 , "294444444", new Date(), "karenenvi@gmail.com", "a12345");
-		Usuario usuarioReceptor = usuarioService.crearUsuario("Claudia Recibe", 32, 5 , "295555555", new Date(), "claudiarecib@gmail.com", "a12345");
-		
-		Integer borigen = usuarioEmisor.getPersona().getBilletera().getBilleteraId();
-		Integer bdestino = usuarioReceptor.getPersona().getBilletera().getBilleteraId();
-		
-		BigDecimal saldoOrigen = usuarioEmisor.getPersona().getBilletera().getCuentaPorMoneda("ARS").getSaldo();
-		BigDecimal saldoDestino = usuarioReceptor.getPersona().getBilletera().getCuentaPorMoneda("ARS").getSaldo();
-
-		billeteraService.enviarSaldo(borigen, usuarioEmisor.getEmail(), "ARS",new BigDecimal(1200), "PRESTAMO", "ya no me debes nada");
-
-		BigDecimal saldoOrigenActualizado = billeteraService.consultarSaldo(borigen, "ARS");
-		BigDecimal saldoDestinoActualizado = billeteraService.consultarSaldo(bdestino, "ARS");
-
-		//AFIRMAMOS QUE, el saldo origen - 1200, sea igual al saldoOrigeActualizado
-		//AFIRMAMOS QUE, el saldo destino + 1200, sea igual al saldoDestinoActualizado
-		System.out.println("saldoORIGEN" + saldoOrigen + "ACTUALIZADO"+saldoOrigenActualizado);
-		System.out.println("saldoDESTNO" + saldoDestino + "ACTUALIZADO"+saldoDestinoActualizado);
-
-		assertTrue(saldoOrigen.subtract(new BigDecimal(1200)).compareTo(saldoOrigenActualizado)==0);
-		assertTrue(saldoDestino.add(new BigDecimal(1200)).compareTo(saldoDestinoActualizado)==0);
-	}
-
 }
