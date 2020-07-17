820ba6ebdaae2ee0ae893f48d208f69ca0751095
Stephanie Castillo <stephaanie.castillo@gmail.com>
2020-07-17 12:32:38 -0300

    Se agrego dependencias al pom

--- billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/DemoApplication.java
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/DemoApplication.java
@@ -2,14 +2,6 @@ package ar.com.ada.api.billeteravirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import ar.com.ada.api.billeteravirtual.security.jwt.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class DemoApplication {
