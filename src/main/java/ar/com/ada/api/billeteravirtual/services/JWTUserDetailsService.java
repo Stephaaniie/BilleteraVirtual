65e28657f6d3cb864f465ed96fc60ad360a2251d
stephaaniie <stephanie.castillo@gmail.com>
2020-07-30 14:27:47 -0300

    Se realizaron los importes necesarios para que el user details pueda ejecutar sin problemas de imports

--- src/main/java/ar/com/ada/api/billeteravirtual/services/JWTUserDetailsService.java
+++ src/main/java/ar/com/ada/api/billeteravirtual/services/JWTUserDetailsService.java
@@ -5,15 +5,18 @@ import org.springframework.stereotype.Service;
 import ar.com.ada.api.billeteravirtual.entities.Usuario;
 import ar.com.ada.api.billeteravirtual.security.jwt.JWTTokenUtil;
 import ar.com.ada.api.billeteravirtual.services.implementations.UsuarioService;
-import io.jsonwebtoken.Claims;
 
+import io.jsonwebtoken.Claims;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Service
 public class JWTUserDetailsService implements UserDetailsService {
@@ -23,6 +26,7 @@ public class JWTUserDetailsService implements UserDetailsService {
     public JWTUserDetailsService(UsuarioService as) {
         this.usuarioService = as;
     }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Usuario u = usuarioService.findByName(username);
