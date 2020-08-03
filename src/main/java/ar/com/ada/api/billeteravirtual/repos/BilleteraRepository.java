78f3601a405516ce198aaa74f5d71961aec4848a
stephaaniie <stephanie.castillo@gmail.com>
2020-08-03 11:16:31 -0300

    Se le retira el metodo del repositorio Billetera que no estaba declarada en la Entity Billetera.

--- src/main/java/ar/com/ada/api/billeteravirtual/repos/BilleteraRepository.java
+++ src/main/java/ar/com/ada/api/billeteravirtual/repos/BilleteraRepository.java
@@ -5,15 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ar.com.ada.api.billeteravirtual.entities.Billetera;
-import ar.com.ada.api.billeteravirtual.entities.Cuenta;
 import ar.com.ada.api.billeteravirtual.entities.Persona;
 
 public interface BilleteraRepository extends JpaRepository<Billetera, Integer> {
     
     List<Billetera> findByPersona(Persona persona);
     
-    List<Billetera> findByCuentaPorMoneda(Cuenta cuentaPorMoneda);
-
     Billetera findByBilleteraId(Integer billeteraId);
     
 }
