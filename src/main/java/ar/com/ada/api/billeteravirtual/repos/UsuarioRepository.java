486dd9bc1d7565b079b4c5dda346335d8ed719fd
stephaaniie <stephanie.castillo@gmail.com>
2020-08-03 11:09:53 -0300

    Se le retira el metodo del repositorio usuario que no estaba declarada en la Entity Usuario.

--- src/main/java/ar/com/ada/api/billeteravirtual/repos/UsuarioRepository.java
+++ src/main/java/ar/com/ada/api/billeteravirtual/repos/UsuarioRepository.java
@@ -6,10 +6,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import ar.com.ada.api.billeteravirtual.entities.Billetera;
 import ar.com.ada.api.billeteravirtual.entities.Persona;
 import ar.com.ada.api.billeteravirtual.entities.Usuario;
-import ar.com.ada.api.billeteravirtual.models.response.LoginResponse;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
@@ -25,8 +23,4 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     List<Usuario> findByFechaLogin(Date fechaLogin);
 
     Usuario findByEmail(String email);
-
-    Usuario login(String username, String password);
-
-	LoginResponse loginResponse(Usuario u, String token, String username);
 }
